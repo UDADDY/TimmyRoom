1a218256ae62aa7204a77dd050561219a3f958df
hyun <hyun@hyunui-MacBookPro.local>
2024-07-28 14:51:53 +0900

    init: 초기 프로젝트

---
+++ src/main/java/com/timmy/TimmyRoom/TimmyRoomApplication.java
@@ -0,0 +1,13 @@
+package com.timmy.TimmyRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimmyRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimmyRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timmy/TimmyRoom/TimmyRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timmy.TimmyRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimmyRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
