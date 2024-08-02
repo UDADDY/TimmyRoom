4d897500a006e719e4d9b7fa3815ba24941d8b04
UDADDY <gustmd5715@gmail.com>
2024-08-02 11:31:57 +0900

    feat: CORS 허용

---
+++ src/main/java/com/timmy/TimmyRoom/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.timmy.TimmyRoom.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "UPDATE", "DELETE", "PUT")
+                .allowCredentials(true);
+    }
+}
