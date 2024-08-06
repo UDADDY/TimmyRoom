ad43ac11d960d3b88d0b394f4d2c8a7d822dc105
UDADDY <gustmd5715@gmail.com>
2024-08-06 15:16:22 +0900

    feat: 파일 업로드 스웨거 작성

--- src/main/java/com/timmy/TimmyRoom/controller/FileController.java
+++ src/main/java/com/timmy/TimmyRoom/controller/FileController.java
@@ -1,7 +1,14 @@
 package com.timmy.TimmyRoom.controller;
 
+import com.timmy.TimmyRoom.gloabl.error.ErrorResponse;
 import com.timmy.TimmyRoom.service.FileService;
 import com.timmy.TimmyRoom.entity.File;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -14,14 +21,35 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/file")
+@Tag(name = "파일")
+@RequiredArgsConstructor
 @Slf4j
 public class FileController {
 
     private final FileService fileService;
 
     @PostMapping("/upload")
+    @Operation(summary = "파일 업로드")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            description = "업로드 성공",
+                            responseCode = "201",
+                            content = {@Content(schema = @Schema(implementation = File.class))}
+                    ),
+                    @ApiResponse(
+                            description = "사용자 인증 실패",
+                            responseCode = "401",
+                            content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}
+                    ),
+                    @ApiResponse(
+                            description = "S3 업로드 실패",
+                            responseCode = "500",
+                            content = {@Content(schema = @Schema(implementation = ErrorResponse.class))}
+                    )
+            }
+    )
     public ResponseEntity<File> uploadFile(
             @AuthenticationPrincipal UserDetails userDetails,
             @RequestParam("file") MultipartFile multipartFile
